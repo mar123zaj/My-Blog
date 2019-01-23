9d99d3984e68b4650eed44100ff4a5b53313f01a
Marcin Zajac <m.zajac995@gmail.com>
2019-01-23 20:38:52 +0100

    Added two models and first view class

--- article/models.py
+++ article/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=150)
+    added = models.DateTimeField(auto_now_add=True, blank=True)
+    text = models.TextField()
+    picture = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Commentary(models.Model):
+    nick = models.CharField(max_length=20)
+    title = models.CharField(max_length=15)
+    comment = models.TextField(max_length=300)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    added = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- article/views.py
+++ article/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Article, Commentary
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+
+    def get_queryset(self):
+        return Article.objects.all()
