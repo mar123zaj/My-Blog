2a960f0fca675aebd47333c884dd49d07d4fc4d7
Marcin Zajac <m.zajac995@gmail.com>
2019-01-24 17:37:21 +0100

    Added DetailView class

--- article/views.py
+++ article/views.py
@@ -9,5 +9,12 @@ class IndexView(generic.ListView):
     queryset = Article.objects.all().order_by('-added')
 
 
+class DetailView(generic.DetailView):
+    model = Article
 
-
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['comments'] = Commentary.objects.all()
+        return context
