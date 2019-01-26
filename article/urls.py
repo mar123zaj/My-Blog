b93786fd2cce4b4267e07ab9aae21c393d60b604
Marcin Zajac <m.zajac995@gmail.com>
2019-01-26 19:51:59 +0100

    Done reset password system and some auth section in base.html

--- article/urls.py
+++ article/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
-from .views import IndexView, DetailView, OutUser, ContactView, PasswordReset
+from django.urls import path, include
+from django.shortcuts import render
+from .views import IndexView, DetailView, OutUser, ContactView, PasswordReset, thanks
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('article/<int:pk>/', DetailView.as_view(), name='detail'),
-    path('accounts/logout/', OutUser.as_view(), name='log'),
     path('contact/', ContactView.as_view(), name='contact'),
+    path('contact/thanks', thanks, name='thanks'),
+    path('accounts/logout/', OutUser.as_view(), name='log'),
     path('accounts/password_reset/', PasswordReset.as_view(), name='reset'),
-
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- article/views.py
+++ article/views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
-from .models import Article, Commentary
+from .models import Article
 from django.views import generic
-from django.contrib.auth.views import LoginView, LogoutView, FormView
-from django.contrib.auth.views import (
-    PasswordResetView,
-    PasswordResetDoneView,
-    PasswordResetConfirmView,
-    PasswordResetCompleteView
-)
-from django.core.mail import send_mail
-from article.forms import ContactForm, PasswordResetForm
+from django.contrib.auth.views import LogoutView, FormView
+from django.contrib.auth.views import PasswordResetView
+from article.forms import ContactForm
 from django.views.generic.edit import FormView
-
+from django.contrib.auth.forms import PasswordResetForm
 
 class IndexView(generic.ListView):
     model = Article
@@ -28,23 +22,21 @@ class OutUser(LogoutView):
 
 
 class PasswordReset(PasswordResetView):
-    form_class =  PasswordResetForm
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        if form.is_valid():
-            form.save()
-            form.send_email()
-            return super().form_valid(form)
+    form_class = PasswordResetForm
+    success_url = '/accounts/password_reset/done/'
 
 
 class ContactView(FormView):
     template_name = 'contact.html'
     form_class = ContactForm
-    success_url = '/thanks/'
+    success_url = '/contact/thanks/'
 
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
         form.send_email()
         return super().form_valid(form)
+
+
+def thanks(request):
+    return render(request, template_name='thanks.html')
